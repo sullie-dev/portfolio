1c76312c3625a7c93eaa91cd70b4dc7d81a70996
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-09-26 16:07:35 +0100

    Add content sections to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,27 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
-      <h1 className="">Hello there</h1>
+    <div>
+      <Head>
+        <title>Sullie | Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Portfolio website for Eoghan O'Sullivan a Cork based web developer"
+        />
+        <meta
+          name="keywords"
+          content="developer, web developer, irish developer, react.js developer"
+        />
+      </Head>
+
+      <div>
+        <div>image</div>
+        <div>about</div>
+      </div>
+      <div>Portfolio cards</div>
+      <div>contact info</div>
+      <div>Footer component</div>
     </div>
   );
 };
