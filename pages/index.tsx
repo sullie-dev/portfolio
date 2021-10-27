98d4d4aa8525ef1588ab37c2a28b235aaf68af10
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-10-27 20:36:45 +0100

    Updated conttac to use IconButton and center items

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ import {
   Heading,
   Wrap,
   WrapItem,
+  Box,
+  IconButton,
+  Center,
 } from "@chakra-ui/react";
 import Head from "next/head";
 import Footer from "../components/Footer";
@@ -19,7 +22,6 @@ import {
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
 import client from "../lib/sanity";
-import { createBreakpoints } from "@chakra-ui/theme-tools";
 
 interface projectProp {
   project_name: string;
@@ -68,29 +70,41 @@ const Home: NextPage = ({ data }: any) => {
           content="developer, web developer, irish developer, react.js developer"
         />
       </Head>
-      <Wrap p="15px">
+      <Wrap py={[2, 4, 6, 8]} px="1">
         <WrapItem>
           <Flex flexDirection="column">
-            <Heading w="650px" h="fit-content" fontSize="6xl" color="gray.800">
+            <Heading
+              w="650px"
+              h="fit-content"
+              fontSize={["2xl", "6xl"]}
+              color="gray.800"
+            >
               Eoghan O Sullivan
             </Heading>
-            <Text fontSize="xl " color="gray.500">
+            <Text fontSize={["l", "2xl"]} color="gray.500">
               Web Developer based in Co.Cork, Ireland
             </Text>
           </Flex>
         </WrapItem>
         <Spacer />
-        <WrapItem>
+        <WrapItem h="100%">
           <Image
             borderRadius="full"
-            boxSize="500px"
+            boxSize={["390px", "500px"]}
             src="/portfolio.jpg"
             alt="Eoghan O'Sullivan"
           />
         </WrapItem>
       </Wrap>
-      <div className="inline-block w-full">
-        <h2 className="text-center mb-12 text-2xl md:text-2xl">Projects</h2>
+      <Wrap>
+        <Heading
+          w="650px"
+          h="fit-content"
+          fontSize={["xl", "5xl"]}
+          color="gray.800"
+        >
+          Projects
+        </Heading>
         {projectData.map((project: projectProp) => (
           <PortfolioCard
             key={project.project_name}
@@ -100,53 +114,93 @@ const Home: NextPage = ({ data }: any) => {
             image={project.image.url}
           />
         ))}
-      </div>
+      </Wrap>
       {/* Contact */}
-      <div className="inline-block mt-5 p-15 h-auto w-full">
-        <h2 className="text-center text-2xl md:text-2xl">Contact me</h2>
 
-        <div className="w-full float-left md:w-6/12 lg:w-3/12">
-          <h3 className="text-l text-center md:text-xl md:w-3/12">
-            <FontAwesomeIcon icon={faEnvelope} /> eoghan@sullie.dev
-          </h3>
-        </div>
-        <div className="w-full float-left md:w-6/12 lg:w-3/12">
-          <a
-            href="http://twitter.com/drunkdarthvader"
-            aria-label="Follow me on Twitter @drunkdarthvader"
-            rel="noreferrer noopener"
-            target="_blank"
-          >
-            <h3 className="text-l text-center md:text-xl md:w-3/12">
-              <FontAwesomeIcon icon={faTwitter} /> @drunkdarthvader
-            </h3>
-          </a>
-        </div>
-        <div className="w-full float-left md:w-6/12 lg:w-3/12">
-          <a
-            href="http://instagram.com/drunkdarkvader"
-            aria-label="Follow my on Instagram @sullie-dev"
-            rel="noreferrer noopener"
-            target="_blank"
-          >
-            <h3 className="text-l text-center md:text-xl md:w-3/12">
-              <FontAwesomeIcon icon={faInstagram} /> @drunkdarkvader
-            </h3>
-          </a>
-        </div>
-        <div className="w-full float-left md:w-6/12 lg:w-3/12">
-          <a
-            href="http://github.com/sullie-dev"
-            aria-label="Follow my on Github @sullie-dev"
-            rel="noreferrer noopener"
-            target="_blank"
-          >
-            <h3 className="text-l text-center text center md:text-xl md:w-3/12">
-              <FontAwesomeIcon icon={faGithub} /> sullie-dev
-            </h3>
-          </a>
-        </div>
-      </div>
+      <Heading
+        w="650px"
+        h="fit-content"
+        fontSize={["xl", "5xl"]}
+        color="gray.800"
+      >
+        Contact me
+      </Heading>
+      <Wrap>
+        <Center w="100vw" h="80px">
+          <WrapItem w={["22%"]}>
+            <Center w="120px" h="80px">
+              <a
+                href="mailto:eoghan@sullie.dev"
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                <Box>
+                  <IconButton
+                    variant="ghost"
+                    fontSize={["24px", "45px"]}
+                    aria-label="Email me on eoghan@sullie.dev"
+                    icon={<FontAwesomeIcon icon={faEnvelope} />}
+                  />
+                </Box>
+              </a>
+            </Center>
+          </WrapItem>
+          <WrapItem w={["22%"]}>
+            <Center w="120px" h="80px">
+              <a
+                href="http://twitter.com/drunkdarthvader"
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                <Box>
+                  <IconButton
+                    variant="ghost"
+                    fontSize={["24px", "45px"]}
+                    aria-label="Follow me on Twitter @drunkdarthvader"
+                    icon={<FontAwesomeIcon icon={faTwitter} />}
+                  />
+                </Box>
+              </a>
+            </Center>
+          </WrapItem>
+          <WrapItem w={["22%"]}>
+            <Center w="120px" h="80px">
+              <a
+                href="http://instagram.com/drunkdarkvader"
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                <Box>
+                  <IconButton
+                    variant="ghost"
+                    fontSize={["24px", "45px"]}
+                    aria-label="Follow my on Instagram @sullie-dev"
+                    icon={<FontAwesomeIcon icon={faInstagram} />}
+                  />
+                </Box>
+              </a>
+            </Center>
+          </WrapItem>
+          <WrapItem w={["22%"]}>
+            <Center w="120px" h="80px">
+              <a
+                href="http://github.com/sullie-dev"
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                <Box>
+                  <IconButton
+                    variant="ghost"
+                    fontSize={["24px", "45px"]}
+                    aria-label="Follow my on Github @sullie-dev"
+                    icon={<FontAwesomeIcon icon={faGithub} />}
+                  />
+                </Box>
+              </a>
+            </Center>
+          </WrapItem>
+        </Center>
+      </Wrap>
       <Footer />
     </div>
   );
