4f0f02abe898f384e38873c5a08b8aebc4544752
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-10-27 20:39:54 +0100

    Create Contact component. Added component to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,21 +7,13 @@ import {
   Heading,
   Wrap,
   WrapItem,
-  Box,
-  IconButton,
-  Center,
 } from "@chakra-ui/react";
 import Head from "next/head";
 import Footer from "../components/Footer";
 import PortfolioCard from "../components/PortfolioCard";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import {
-  faInstagram,
-  faTwitter,
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons";
+
 import client from "../lib/sanity";
+import Contact from "../components/Contact";
 
 interface projectProp {
   project_name: string;
@@ -115,92 +107,7 @@ const Home: NextPage = ({ data }: any) => {
           />
         ))}
       </Wrap>
-      {/* Contact */}
-
-      <Heading
-        w="650px"
-        h="fit-content"
-        fontSize={["xl", "5xl"]}
-        color="gray.800"
-      >
-        Contact me
-      </Heading>
-      <Wrap>
-        <Center w="100vw" h="80px">
-          <WrapItem w={["22%"]}>
-            <Center w="120px" h="80px">
-              <a
-                href="mailto:eoghan@sullie.dev"
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                <Box>
-                  <IconButton
-                    variant="ghost"
-                    fontSize={["24px", "45px"]}
-                    aria-label="Email me on eoghan@sullie.dev"
-                    icon={<FontAwesomeIcon icon={faEnvelope} />}
-                  />
-                </Box>
-              </a>
-            </Center>
-          </WrapItem>
-          <WrapItem w={["22%"]}>
-            <Center w="120px" h="80px">
-              <a
-                href="http://twitter.com/drunkdarthvader"
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                <Box>
-                  <IconButton
-                    variant="ghost"
-                    fontSize={["24px", "45px"]}
-                    aria-label="Follow me on Twitter @drunkdarthvader"
-                    icon={<FontAwesomeIcon icon={faTwitter} />}
-                  />
-                </Box>
-              </a>
-            </Center>
-          </WrapItem>
-          <WrapItem w={["22%"]}>
-            <Center w="120px" h="80px">
-              <a
-                href="http://instagram.com/drunkdarkvader"
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                <Box>
-                  <IconButton
-                    variant="ghost"
-                    fontSize={["24px", "45px"]}
-                    aria-label="Follow my on Instagram @sullie-dev"
-                    icon={<FontAwesomeIcon icon={faInstagram} />}
-                  />
-                </Box>
-              </a>
-            </Center>
-          </WrapItem>
-          <WrapItem w={["22%"]}>
-            <Center w="120px" h="80px">
-              <a
-                href="http://github.com/sullie-dev"
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                <Box>
-                  <IconButton
-                    variant="ghost"
-                    fontSize={["24px", "45px"]}
-                    aria-label="Follow my on Github @sullie-dev"
-                    icon={<FontAwesomeIcon icon={faGithub} />}
-                  />
-                </Box>
-              </a>
-            </Center>
-          </WrapItem>
-        </Center>
-      </Wrap>
+      <Contact />
       <Footer />
     </div>
   );
